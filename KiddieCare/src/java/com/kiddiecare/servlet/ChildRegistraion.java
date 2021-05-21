7d7f0acc30d77a2de7f7609b995465fa96bb42c8
Poorna Senani Gamage <59789032+NotitaZone@users.noreply.github.com>
2021-05-21 07:36:29 +0530

    update child registraion

--- KiddieCare/src/java/com/kiddiecare/servlet/ChildRegistraion.java
+++ KiddieCare/src/java/com/kiddiecare/servlet/ChildRegistraion.java
@@ -69,28 +69,19 @@ public class ChildRegistraion extends HttpServlet {
             String childName = request.getParameter("childname");
             String gender = request.getParameter("gender");
             String dob = request.getParameter("childbirthday");
-            String age = request.getParameter("childage");
-            String month = request.getParameter("month");
-            String days = request.getParameter("days");
-            String weight = request.getParameter("birthweight");
-            String length = request.getParameter("birthlength");
 
             String sqlquery1 = "INSERT INTO user (nic,guardian_name,email,contact_no,address,mother_name,age,number_of_children) VALUES ('" + nic + "','" + guardianName + "','" + email + "','" + contactNo + "','" + address + "',"
                     + "'" + motherName + "','" + motherAge + "','" + numberOfChildren + "')";
 
-            String sqlquery2 = "INSERT INTO child (child_birth_register_no, user_register_no, health_physician_no, family_health_physician_no, child_name, gender, dob, age, month, days, weight, length) VALUES('" + childBirthRegisterNo + "',(select max(user_register_no)  from chdr.user where user.nic = '"+nic+"'),'" + healthPhysicianNo + "','" + familyHealthPhysicianNo + "','" + childName + "',"
-                    + "'" + gender + "','" + dob + "','" + age + "','" + month + "','" + days + "','" + weight + "','" + length + "')";
+            String sqlquery2 = "INSERT INTO child (child_birth_register_no, user_register_no, health_physician_no, family_health_physician_no, child_name, gender, dob) VALUES('" + childBirthRegisterNo + "',(select max(user_register_no)  from chdr.user where user.nic = '"+nic+"'),'" + healthPhysicianNo + "','" + familyHealthPhysicianNo + "','" + childName + "',"
+                    + "'" + gender + "','" + dob + "')";
 //insert into chdr.child values ('63924',(select user_register_no from chdr.user where user.nic = '547410017'),'34928','3279','Poorna','girl','1992/04/16',1,1,2,2.5,50);
             
             
             String sqlquery3 = "INSERT INTO login VALUES('" + nic + "','" + password + "')";
 
-//            queryDAO.save(sqlquery1);
-//            queryDAO.save(sqlquery2);
-//            queryDAO.save(sqlquery3);
-            
             if (queryDAO.save(sqlquery1)&& (queryDAO.save(sqlquery2) && (queryDAO.save(sqlquery3)))) {
-                response.sendRedirect(request.getContextPath()+"AdminPanel/AdminDashboard.jsp");
+                response.sendRedirect(request.getContextPath()+"/AdminPanel/AdminDashboard.jsp");
                 
             } else {
                 response.sendRedirect("Error.jsp");
